e541141ac93e3d71b2971863fc9551db6cb5e300
Mahmood-elbadri <61979779+Mahmood-elbadri@users.noreply.github.com>
2023-11-10 10:58:15 +0200

    Update AddToCartPage.java

--- src/test/java/Pages/AddToCartPage.java
+++ src/test/java/Pages/AddToCartPage.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class AddToCartPage extends BasePage{
-    WebDriver driver;
     @FindBy(xpath = "(//a[@class='button'][normalize-space()='Add to Cart'])[1]")
     private WebElement malePuppy;
     public AddToCartPage(WebDriver driver){
